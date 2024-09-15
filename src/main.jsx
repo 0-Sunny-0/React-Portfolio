dc0e3a0022fddfac5e7e498fdbe0c1e5ffe39687
Sunny Le Bunz <msafinch@gmail.com>
2024-09-15 14:10:43 -0400

    main.jsx updated with browser router and router provider routing code

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,46 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// sets up application routing behavior
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// importing css styling
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+// importing in the pages that the router will use to show the proper views.
+// Remember to import ALL from pages folders
+import App from './App';
+// import AboutMe from "";
+// import Portfolio from "";
+// import Contact from "";
+// import Resume from "":
+
+// Define accessible routes, and which components respond to which URLs
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <AboutMe />
+      },
+      {
+        path: '/portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: './contact',
+        element: <Contact />,
+      },
+      {
+        path: './resume',
+        element: <Resume />,
+      },
+    ],
+  },
+]);
+
+// Render the RouterProvider component
+ReactDom.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
