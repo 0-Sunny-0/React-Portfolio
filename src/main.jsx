3fc3a0caf05fc8b5f913573c2d612b3296cf2be9
Sunny Le Bunz <msafinch@gmail.com>
2024-09-15 14:25:33 -0400

    additional files and folders added for proper routing and page rendering

--- src/main.jsx
+++ src/main.jsx
@@ -22,7 +22,7 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <AboutMe />
+        element: <AboutHer />
       },
       {
         path: '/portfolio',
