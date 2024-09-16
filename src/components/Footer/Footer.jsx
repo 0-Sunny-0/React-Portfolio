ea0eb9ac29d1c4c9b7c0789dc59319cfef9d3dee
Sunny Le Bunz <msafinch@gmail.com>
2024-09-16 00:30:52 -0400

    contact form added, without logic. Css is thrown off for footer padding on two pages. Will come back to fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,13 +5,15 @@ import './Footer.css';
 export default function Footer() {
     return (
         <>
-          <footer class="footer">
-            <div class="container text-center">
-                <a href="https://github.com/0-Sunny-0"><i class="fa fa-github"></i></a>
-                <a href="https://www.linkedin.com/in/sunny-finch/"><i class="fa fa-linkedin"></i></a>
-            </div><br></br>
-            <p>Made with 💛 by Sunny-0</p>
-            </footer>  
+            <div>
+                <footer class="footer">
+                    <div class="container text-center">
+                        <a href="https://github.com/0-Sunny-0"><i class="fa fa-github"></i></a>
+                        <a href="https://www.linkedin.com/in/sunny-finch/"><i class="fa fa-linkedin"></i></a>
+                    </div><br></br>
+                    <p>Made with 💛 by Sunny-0</p>
+                </footer>
+            </div> 
         </>
     );
 };
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -4,7 +4,21 @@ import './Contact.css';
 function Contact() {
     return (
         <div id='container'>
-            <p>Sample text</p>
+            <div id='contactForm' className="form-floating mb-3">
+                <input type="name" className="form-control" id="floatingInput" placeholder="Name?" />
+                <label for="floatingInput">Name:</label>
+            </div>
+            <div id='contactForm' className="form-floating mb-3">
+                <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                <label for="floatingInput">Email address</label>
+            </div>
+            <div className="form-floating">
+                <input type="text" className="form-control" id="textBox" placeholder="Send me a message!" />
+                <label for="textBox">Send me a message!</label>
+            </div>
+            <div id='submitBtn'>
+                <button id='btnSubmit' className="btn btn-primary w-100 py-2 align-items-center" type="submit">Submit</button>
+            </div>
         </div>
     );
 };
